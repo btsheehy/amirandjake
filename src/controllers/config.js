740561466944936784e412533c5e8143cdfc2640
Brandon Sheehy <sheehy.brandon@gmail.com>
2018-06-27 22:56:08 -0500

    new api

--- src/controllers/config.js
+++ src/controllers/config.js
@@ -1,3 +1,4 @@
-// const apiUrl = 'http://localhost:3000/'
-const apiUrl = 'http://ec2-54-201-9-185.us-west-2.compute.amazonaws.com/'
+// const apiUrl = 'http://localhost:80/'
+// const apiUrl = 'http://ec2-54-201-9-185.us-west-2.compute.amazonaws.com/'
+const apiUrl = 'https://amirandjake.com/'
 export default apiUrl
