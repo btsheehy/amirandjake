817f7d2f22737844c6d4df845d3bc44abe1c0aba
Brandon Sheehy <sheehy.brandon@gmail.com>
2018-07-02 22:47:20 -0500

    fix config to read environment

--- src/controllers/config.js
+++ src/controllers/config.js
@@ -1,4 +1,7 @@
-// const apiUrl = 'http://localhost:80/'
+if (/localhost/i.test(window.location.hostname)) {
+	const apiUrl = 'http://localhost:80/'
+} else {
+	const apiUrl = 'https://amirandjake.com/'
+}
 // const apiUrl = 'http://ec2-54-201-9-185.us-west-2.compute.amazonaws.com/'
-const apiUrl = 'https://amirandjake.com/'
 export default apiUrl
