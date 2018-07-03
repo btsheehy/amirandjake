c065935efb63a88ace6bf2631e5a5df66093a6a8
Brandon Sheehy <sheehy.brandon@gmail.com>
2018-07-02 22:48:08 -0500

    config

--- src/controllers/config.js
+++ src/controllers/config.js
@@ -1,7 +1,8 @@
+let apiUrl
 if (/localhost/i.test(window.location.hostname)) {
-	const apiUrl = 'http://localhost:80/'
+	apiUrl = 'http://localhost:80/'
 } else {
-	const apiUrl = 'https://amirandjake.com/'
+	apiUrl = 'https://amirandjake.com/'
 }
 // const apiUrl = 'http://ec2-54-201-9-185.us-west-2.compute.amazonaws.com/'
 export default apiUrl
