1edee4738d64dcf9fcb65ebbd6b278e873958489
Brandon Sheehy <sheehy.brandon@gmail.com>
2020-11-19 05:24:31 -0600

    api gateway

--- src/controllers/config.js
+++ src/controllers/config.js
@@ -1,9 +1,9 @@
-let apiUrl;
+let apiUrl
 if (/localhost/i.test(window.location.hostname)) {
-  apiUrl = "http://localhost:80/";
+  apiUrl = 'http://localhost:80/'
 } else {
   // apiUrl = 'https://jakeandamir.app/'
-  apiUrl = "http://ec2-54-201-208-75.us-west-2.compute.amazonaws.com/";
+  apiUrl = 'https://lqhr8y7kl0.execute-api.us-west-2.amazonaws.com/prod/'
 }
 // const apiUrl = 'http://ec2-54-201-9-185.us-west-2.compute.amazonaws.com/'
-export default apiUrl;
+export default apiUrl
